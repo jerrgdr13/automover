a84f7e2d70488896a16aaa522d9acf0f9ca0dbe8
Jerry Guerra <Jerry Guera>
2020-09-23 11:12:47 -0600

    Second change

--- Extension_Searcher.py
+++ Extension_Searcher.py
@@ -1,6 +1,12 @@
 import os
 
+path = '/Users/jguerrad/'
+
+l = os.listdir(path)
+for e in l:
+    print(e)
+
 for root, dirs, files in os.walk('/Users/jguerrad/Downloads/'):
     for file in files:
-        if file.endswith('.dmg'):
+        if file.endswith('.*'):
             print(os.path.join(root, file))
