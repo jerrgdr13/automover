9568a23e8a9aaf4dfa8077cf4a43a3e141892552
Jerry Guerra <Jerry Guera>
2020-09-23 11:17:28 -0600

    4

--- Extension_Searcher.py
+++ Extension_Searcher.py
@@ -5,8 +5,3 @@ path = '/Users/jguerrad/Downloads/'
 l = os.listdir(path)
 for e in l:
     print(e)
-
-for root, dirs, files in os.walk('/Users/jguerrad/Downloads/'):
-    for file in files:
-        if file.endswith('.*'):
-            print(os.path.join(root, file))
