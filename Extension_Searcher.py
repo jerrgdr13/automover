7c43fac18e2d2c088faee46764d6a22d6e055e2c
Jerry Guerra <Jerry Guera>
2020-10-01 20:29:30 -0600

    reading JSON

--- Extension_Searcher.py
+++ Extension_Searcher.py
@@ -6,5 +6,10 @@ myjsonfile=open('./information.json','r')
 jsondata=myjsonfile.read()
 
 #parse Json
-obj=json.load(jsondata)
-print(str(obj['Origin_Path'])
+obj=json.loads(jsondata)
+#print(str(obj['Origin_Path']))
+
+org_path=str(obj['Origin_Path']) 
+l = os.listdir(org_path)
+for e in l:
+    print(e)
