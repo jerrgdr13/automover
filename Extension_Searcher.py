89a70180bf0b36c3ae37e889090cb01fb2b5cf80
Jerry Guerra <Jerry Guera>
2020-10-02 11:19:24 -0600

    02oct

--- Extension_Searcher.py
+++ Extension_Searcher.py
@@ -12,4 +12,14 @@ obj=json.loads(jsondata)
 org_path=str(obj['Origin_Path']) 
 l = os.listdir(org_path)
 for e in l:
-    print(f"file name{e} and the extension is {e.split('.')[1]}")
+    if e.split('.')[1] == True:
+        print(str(e.split('.')[1]))
+        print(str(e.split('.')[0]))
+        #print(file_name)
+        #print(ext_file)
+    #print(f"file name{e} and the extension is {e.split('.')[1]}")
+
+
+#def  ext_validator():
+#    for i in range(len(ext_lst)):
+#        print(i)
