dba4e19193f2099ad1d44d966d29a3ad3d45940e
Jerry Guerra <Jerry Guera>
2020-10-02 12:05:21 -0600

    opt3

--- Extension_Searcher.py
+++ Extension_Searcher.py
@@ -12,11 +12,13 @@ obj=json.loads(jsondata)
 org_path=str(obj['origin_path']) 
 l = os.listdir(org_path)
 for e in l:
-    if e.split('.')[1] == True:
-        print(str(e.split('.')[1]))
-        print(str(e.split('.')[0]))
-        #print(file_name)
-        #print(ext_file)
+    if str(e.split('.')[0]) in (None,""):
+        print('Not a File')
+    else:
+        file_name=str(e.split('.')[0]),
+        ext_name=str(e.split('.')[1])
+        print(file_name)
+        print(ext_name)
     #print(f"file name{e} and the extension is {e.split('.')[1]}")
 
 
