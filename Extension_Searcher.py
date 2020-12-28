4d0b2df4d94ec707fae550b2ed2137d27a5812a9
Jerry Guerra <Jerry Guera>
2020-12-28 10:25:45 -0600

    commit

--- Extension_Searcher.py
+++ Extension_Searcher.py
@@ -28,25 +28,25 @@ def move_files(source_folder, dest_folder):
         else:
             print(e)
 
-    #the following will create the dest_path
-    def dest_dir(dest_folder):
-            for path, dir, files in os.walk.exists(dest_folder):
-                    try:
-                        extFile = ext_file(org_path)
-                        if not os.path.exists(extFile):
-                            os.makedirs(extFile)
-                            return dest_folder + '/' + extFile + '%Y-%m-%d %H:%M:%S'
-                        else: #os.path.exists(ext_file):
-                            dest_dir = dest_folder + '/' + extFile + '%Y-%m-%d %H:%M:%S'
-                            return dest_dir
-       
-        #The following will check the files extension
-        def ext_file(source_folder):
-            for e in org_path:
-                if str(e.split('.')[0]) in (None,""):
-                    print('No a File')
-                else:
-                    return (str(e.split('.')[1]))
+#the following will create the dest_path
+def dest_dir(dest_folder):
+        for path, dir, files in os.walk.exists(dest_folder):
+                try:
+                    extFile = ext_file(org_path)
+                    if not os.path.exists(extFile):
+                        os.makedirs(extFile)
+                        return dest_folder + '/' + extFile + '%Y-%m-%d %H:%M:%S'
+                    else: #os.path.exists(ext_file):
+                        dest_dir = dest_folder + '/' + extFile + '%Y-%m-%d %H:%M:%S'
+                        return dest_dir
+   
+#The following will check the files extension
+def ext_file(source_folder):
+    for e in org_path:
+        if str(e.split('.')[0]) in (None,""):
+            print('No a File')
+        else:
+            return (str(e.split('.')[1]))
 
 
 
